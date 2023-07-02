913290659785f7aadae58659417cc283b2cd2daa
alisalimi77 <ali.salimi77@gmail.com>
2023-07-02 10:05:29 +0330

    modify main.py , add some part for data information

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import pandas as pd 
 import numpy as np 
 import matplotlib.pyplot as plt
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.impute import SimpleImputer
+from sklearn.model_selection import train_test_split
 
 
-dataset = pd.read_csv("bigmart.csv")
+dataset = pd.read_csv("bigmart.csv") #import dataset bigmart_sale 
 
-print(dataset.head())
-dataset.tail()
-print(dataset.describe())
-
+print("------Data Shape-----\n\n",dataset.shape)
+print("\n\n-----Data head----- \n\n",dataset.head())
+print("\n\n\n-----data information-----\n",dataset.info)
+print("\n\n\n Data size :>>>>> ",dataset.size)
 
