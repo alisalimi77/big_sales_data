bc4ff825e661728b36d67068abfa28a58271d29d
alisalimi77 <ali.salimi77@gmail.com>
2023-07-05 11:28:31 +0330

    add Preprocess class to prepro.py object_to_numeric ,  handle_null_values , standardize_dataset ,normalize_dataset

--- main.py
+++ main.py
@@ -5,7 +5,13 @@ from sklearn.preprocessing import StandardScaler, OneHotEncoder
 from sklearn.impute import SimpleImputer
 from sklearn.model_selection import train_test_split
 from analyze import analyze_dataset #Import analyze.py . check the analyze.py
+from prepro import object_to_numeric 
+from prepro import null_values 
 
 dataset = pd.read_csv("bigmart.csv") #ّّImport dataset bigmart_sale 
 
 analyze_dataset(dataset)  #It does as the name suggests 
+
+dataset = object_to_numeric(dataset) #It does as the name suggests 
+dataset = null_values(dataset,-1)
+analyze_dataset(dataset)
