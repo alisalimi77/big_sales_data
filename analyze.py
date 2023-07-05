1a2023baf6d4cdbb279197dae9cec531cdad070b
alisalimi77 <ali.salimi77@gmail.com>
2023-07-05 11:34:21 +0330

    add data visualization in analyze.py

--- analyze.py
+++ analyze.py
@@ -1,8 +1,14 @@
-
-import pandas as pd 
-import numpy as np 
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 
 def analyze_dataset(dataset):
+    """
+    Analyze the dataset by displaying various information, statistics, and visualizations.
+
+    Parameters:
+        dataset (pandas.DataFrame): The input dataset to be analyzed.
+    """
     # Display the shape of the dataset
     print("------Data Shape-----\n\n", dataset.shape)
 
@@ -26,3 +32,21 @@ def analyze_dataset(dataset):
     # Count the number of missing values in each column
     missing_values = dataset.isnull().sum()
     print("\n-----Missing Values-----\n", missing_values)
+
+    # Visualize missing values
+    plt.figure(figsize=(10, 6))
+    missing_values.plot(kind="bar")
+    plt.title("Missing Values")
+    plt.xlabel("Columns")
+    plt.ylabel("Count")
+    plt.show()
+
+    # Visualize distribution of numeric columns
+    numeric_columns = dataset.select_dtypes(include=np.number).columns
+    for column in numeric_columns:
+        plt.figure(figsize=(8, 6))
+        dataset[column].plot(kind="hist")
+        plt.title(f"Distribution of {column}")
+        plt.xlabel(column)
+        plt.ylabel("Frequency")
+        plt.show()
