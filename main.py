4ff80fde9cf16daac49542f3fb8e9bf6007d52eb
alisalimi77 <ali.salimi77@gmail.com>
2023-07-12 18:10:47 +0330

    Ali modify main.py

--- main.py
+++ main.py
@@ -12,12 +12,12 @@ dataset = pd.read_csv("bigmart.csv") #ّّImport dataset for example bigmart_sal
 
 analyze_dataset(dataset)  #It does as the name suggests 
 
-# preprocess = DataPreprocessor()
+preprocess = DataPreprocessor()
 
-# prepro_dataset = preprocess.object_to_numeric(dataset) #It does as the name suggests
-# prepro_dataset = preprocess.handle_null_values(prepro_dataset,-1) #Check the null values are not null values 
-# prepro_dataset = preprocess.standardize_dataset(prepro_dataset) #Standardization the dataset     
-# prepro_dataset = preprocess.normalize_dataset(prepro_dataset) #Normalaization.
+prepro_dataset = preprocess.object_to_numeric(dataset) #It does as the name suggests
+prepro_dataset = preprocess.handle_null_values(prepro_dataset,-1) #Check the null values are not null values 
+prepro_dataset = preprocess.standardize_dataset(prepro_dataset) #Standardization the dataset     
+prepro_dataset = preprocess.normalize_dataset(prepro_dataset) #Normalaization.
 
-# analyze_dataset(prepro_dataset)
+analyze_dataset(prepro_dataset)
 
