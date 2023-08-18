725cc232a1e8f4b228da10d6c0fadd37ffe8191e
alisalimi77 <ali.salimi77@gmail.com>
2023-08-18 22:04:47 +0330

    modifed main.py & analyze.py

--- analyze.py
+++ analyze.py
@@ -1,52 +1,21 @@
-import pandas as pd
-import numpy as np
+# analyze.py
+
 import matplotlib.pyplot as plt
+import seaborn as sns
 
-def analyze_dataset(dataset):
-    """
-    Analyze the dataset by displaying various information, statistics, and visualizations.
+def plot_regression_actual_vs_predicted(y_test, y_pred):
+   plt.scatter(y_test, y_pred)
+   plt.xlabel('Actual Values')
+   plt.ylabel('Predicted Values')
+   plt.title('Actual vs Predicted Values')
+   plt.show()
 
-    Parameters:
-        dataset (pandas.DataFrame): The input dataset to be analyzed.
-    """
-    # Display the shape of the dataset
-    print("------Data Shape-----\n\n", dataset.shape)
+def plot_residuals(y_test, y_pred):
+   residuals = y_test - y_pred
+   sns.displot(residuals)
+   plt.title('Residuals Distribution')
+   plt.show()
 
-    # Display the first few rows of the dataset
-    print("\n\n-----Data head----- \n\n", dataset.head())
-
-    # Display information about the dataset
-    print("\n\n\n-----Data information-----\n")
-    print(dataset.info())
-
-    # Display the size of the dataset
-    print("\n\n\n Data size :>>>>> ", dataset.size)
-
-    # Display the column names of the dataset
-    print("\n\n-----Column names----\n\n", dataset.columns)
-
-    # Display descriptive statistics of the dataset
-    print("\n\n-----Descriptive statistics----\n\n")
-    print(dataset.describe(include="all"))
-
-    # Count the number of missing values in each column
-    missing_values = dataset.isnull().sum()
-    print("\n-----Missing Values-----\n", missing_values)
-
-    # Visualize missing values
-    plt.figure(figsize=(10, 6))
-    missing_values.plot(kind="bar")
-    plt.title("Missing Values")
-    plt.xlabel("Columns")
-    plt.ylabel("Count")
-    plt.show()
-
-    # Visualize distribution of numeric columns
-    numeric_columns = dataset.select_dtypes(include=np.number).columns
-    for column in numeric_columns:
-        plt.figure(figsize=(8, 6))
-        dataset[column].plot(kind="hist")
-        plt.title(f"Distribution of {column}")
-        plt.xlabel(column)
-        plt.ylabel("Frequency")
-        plt.show()
+def regression_model_analysis(y_test, y_pred):
+   plot_regression_actual_vs_predicted(y_test, y_pred)
+   plot_residuals(y_test, y_pred)
--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import pandas as pd
-from sklearn.model_selection import train_test_split
-from analyze import analyze_dataset
+from sklearn.model_selection import train_test_split , GridSearchCV
+from analyze import regression_model_analysis
 from prepro import DataPreprocessor
-from regression import RegressionModel
+from regression import RegressionModel 
+import numpy as np
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 
 # Load dataset
 dataset = pd.read_csv("bigmart.csv")
@@ -16,8 +18,8 @@ preprocessor = DataPreprocessor()
 # Preprocess data
 preprocessed_dataset = preprocessor.object_to_numeric(dataset)
 preprocessed_dataset = preprocessor.handle_null_values(preprocessed_dataset, -1)
-preprocessed_dataset = preprocessor.standardize_dataset(preprocessed_dataset)
 preprocessed_dataset = preprocessor.normalize_dataset(preprocessed_dataset)
+preprocessed_dataset = preprocessor.standardize_dataset(preprocessed_dataset)
 
 # Split data into features (X) and target (y)
 X = preprocessed_dataset.iloc[:, :-1].values
@@ -27,7 +29,7 @@ y = preprocessed_dataset.iloc[:, -1].values
 # X_selected = preprocessor.feature_selection(X, y, k=10)  # Adjust k as needed
 
 # Split data into training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2 , random_state=42)
 
 # Initialize separate RegressionModel instances for each model
 regressor_linear = RegressionModel()
@@ -46,15 +48,50 @@ models = {
     "Random Forest Regression": regressor_random_forest
 }
 
+metrics = {}
+
 for name, model in models.items():
-    y_pred = model.test_regression_model(X_test)
-    mse, mae, rmse, r2 = model.calculate_regression_metrics(y_test, y_pred)
+   # Get metrics
+   mse, mae, rmse, r2 = model.test_and_evaluate(X_test, y_test)
+   
+   metrics[name] = {
+      'mse': mse, 
+      'mae': mae,
+      'rmse': rmse,
+      'r2': r2
+   }
 
-    print(f"Evaluating {name} model:")
-    print("MSE:", mse)
-    print("MAE:", mae)
-    print("RMSE:", rmse)
-    print("R-squared:", r2)
-    print()
+# Select best model based on rmse
+best_model = min(metrics, key=lambda model: metrics[model]['rmse'])
+print("Best model based on RMSE:", best_model)
 
-print("Done")
+# Tune hyperparameters of best model using GridSearchCV
+if best_model == "Random Forest Regression":
+   param_grid = {
+      'n_estimators': [100, 200, 500],
+      'max_depth': [5, 8, 15]
+   }
+
+   grid_search = GridSearchCV(models[best_model].regression_model, param_grid)
+   grid_search.fit(X_train, y_train)
+   
+   print("Best hyperparameters:", grid_search.best_params_)
+   
+   models[best_model].regression_model = grid_search.best_estimator_
+   # Evaluate each model
+for name, model in models.items():
+   y_pred = model.test_regression_model(X_test)
+   print(name, "performance:")
+   
+   # Pass prediction to analysis
+   regression_model_analysis(y_test, y_pred)
+   rmse = np.sqrt(mean_squared_error(y_test, y_pred))  
+   mae = mean_absolute_error(y_test, y_pred)
+   r2 = r2_score(y_test, y_pred)
+
+   print(name)
+   print("- RMSE: ", rmse)
+   print("- MAE: ", mae)
+   print("- R-squared: ", r2)
+
+   print()
