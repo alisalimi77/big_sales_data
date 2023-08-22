639cfdd3b999aa6dae5987e2e03ad6dbf41dd711
alisalimi77 <ali.salimi77@gmail.com>
2023-08-22 11:09:59 +0330

    increase code readablity

--- analyze.py
+++ analyze.py
@@ -1,7 +1,48 @@
-# analyze.py
-
-import matplotlib.pyplot as plt
+import pandas as pd
 import seaborn as sns
+import matplotlib.pyplot as plt
+import numpy as np
+
+def analyze_dataset(dataset):
+    # Display dataset head
+    print(dataset.head())
+    # Display dataset information
+    print(dataset.info())
+    # Display descriptive statistics
+    print(dataset.describe().transpose())
+
+    # Check for null values
+    null_counts = dataset.isnull().sum()
+    # Create a dataframe to show null statistics
+    null_df = pd.DataFrame(null_counts, columns=['Number of Nulls'])
+    total_cells = np.product(dataset.shape)
+    null_df['Percent Null'] = null_df['Number of Nulls'] / total_cells * 100
+    # Display null statistics
+    print("Null Value Analysis:\n")
+    print(null_df)
+
+    # Show duplicate data
+    print(dataset[dataset.duplicated()])
+
+    # Data Visualization
+    plt.figure(figsize=(10, 20))
+    for i, col in enumerate(['Outlet_Size', 'Outlet_Location_Type', 'Item_Fat_Content'], 1):
+        plt.subplot(3, 2, i)
+        sns.countplot(x=col, data=dataset, palette='Set2')
+        plt.subplot(3, 2, i + 1)
+        sns.countplot(x=col, hue='Outlet_Type', data=dataset, palette='Set2')
+    plt.tight_layout()
+    plt.show()
+
+    # Countplot for establishment year distribution
+    fig, ax = plt.subplots(figsize=(6, 6))
+    sns.countplot(data=dataset, x='Outlet_Establishment_Year', ax=ax)
+    ax.set_ylabel('Frequency')
+    ax.set_xlabel('Establishment Year')
+    ax.set_title('Outlet Establishment Year Distribution')
+    plt.tight_layout()
+    plt.show()
+
 
 def plot_regression_actual_vs_predicted(y_test, y_pred):
    plt.scatter(y_test, y_pred)
@@ -19,3 +60,19 @@ def plot_residuals(y_test, y_pred):
 def regression_model_analysis(y_test, y_pred):
    plot_regression_actual_vs_predicted(y_test, y_pred)
    plot_residuals(y_test, y_pred)
+
+
+def null_values(dataset):
+    # Calculate null values
+    null_counts = dataset.isnull().sum()
+
+    # Create dataframe with column names
+    null_df = pd.DataFrame(null_counts, columns=['Number of Nulls'])  
+
+    # Calculate null percentage
+    total_cells = np.product(dataset.shape)
+    null_df['Percent Null'] = null_df['Number of Nulls'] / total_cells * 100
+
+    # Display dataframe
+    print("Null Value Analysis:\n")
+    print(null_df)
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split , GridSearchCV
-from analyze import regression_model_analysis
+import analyze 
 from prepro import DataPreprocessor
 from regression import RegressionModel 
 import numpy as np
@@ -13,86 +13,33 @@ from prettytable import PrettyTable
 # Load dataset
 dataset = pd.read_csv("bigmart.csv")
 
-# Analyze dataset (if needed)
-# Data info & head
-print(dataset.head())
-print(dataset.info())
-# Data describe
-print(dataset.describe().transpose())
+# Analyze data if you need it 
 
-#null Data?
-# Calculate null values
-null_counts = dataset.isnull().sum()
+analyze.analyze_dataset(dataset)
 
-# Create dataframe with column names
-null_df = pd.DataFrame(null_counts, columns=['Number of Nulls'])  
-
-# Calculate null percentage
-total_cells = np.product(dataset.shape)
-null_df['Percent Null'] = null_df['Number of Nulls'] / total_cells * 100
-
-# Display dataframe
-print("Null Value Analysis:\n")
-print(null_df)
-
-# Show Data contain Duplicate data
-print(dataset[dataset.duplicated()])
-
-#Data Visualization
-plt.figure(figsize=(10,20)) 
-
-# Loop through columns to plot
-for i, col in enumerate(['Outlet_Size', 'Outlet_Location_Type', 'Item_Fat_Content'], 1):
-
-  # Subplot for count plot 
-  plt.subplot(3,2,i)
-  sns.countplot(x=col, data=dataset, palette='Set2')
-
-  # Subplot for count plot by outlet type
-  plt.subplot(3,2,i+1) 
-  sns.countplot(x=col, hue='Outlet_Type', data=dataset, palette='Set2')
-
-# Show plot  
-plt.tight_layout()
-plt.show()
-
-fig, ax = plt.subplots(figsize=(6,6))
-
-# Countplot 
-sns.countplot(
-    data=dataset,
-    x='Outlet_Establishment_Year',
-    ax=ax
-)
-
-# Labels and title
-ax.set_ylabel('Frequency')
-ax.set_xlabel('Establishment Year')
-ax.set_title('Outlet Establishment Year Distribution')
-
-# Display plot
-plt.tight_layout()
-plt.show()
 # Initialize DataPreprocessor
 preprocessor = DataPreprocessor()
 
 # Preprocess data
 preprocessed_dataset = preprocessor.object_to_numeric(dataset)
+
 print(preprocessed_dataset.head())
-preprocessed_dataset = preprocessor.handle_null_values(preprocessed_dataset,'median')
-# Calculate null values
-null_counts = preprocessed_dataset.isnull().sum()
 
-# Create dataframe with column names
-null_df = pd.DataFrame(null_counts, columns=['Number of Nulls'])  
-
-# Calculate null percentage
-total_cells = np.product(preprocessed_dataset.shape)
-null_df['Percent Null'] = null_df['Number of Nulls'] / total_cells * 100
-
-# Display dataframe
+# check null valeus
 print("Null Value Analysis:\n")
-print(null_df)
+analyze.null_values(preprocessed_dataset)
+
+# Handle null values
+
+preprocessed_dataset = preprocessor.handle_null_values(preprocessed_dataset,'median')
+
+# check null values
+print("Null Value Analysis after handeling :\n")
+analyze.null_values(preprocessed_dataset)
+
+
+
+
 preprocessed_dataset = preprocessor.normalize_dataset(preprocessed_dataset)
 preprocessed_dataset = preprocessor.standardize_dataset(preprocessed_dataset)
 
@@ -107,65 +54,102 @@ y = preprocessed_dataset.iloc[:, -1].values
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2 , random_state=42)
 
 
-print('X_train shape is ' , X_train.shape)
-print('X_test shape is ' , X_test.shape)
-print('y_train shape is ' , y_train.shape)
-print('y_test shape is ' , y_test.shape)
+print('X_train shape is :\n' , X_train.shape)
+print('X_test shape is :\n' , X_test.shape)
+print('y_train shape is :\n' , y_train.shape)
+print('y_test shape is :\n' , y_test.shape)
+print("\n\n")
+
+
 # Initialize separate RegressionModel instances for each model
+
 regressor_linear = RegressionModel()
 
 regressor_decision_tree = RegressionModel()
+
 regressor_random_forest = RegressionModel()
+
 OrthogonalMatchingPursuit = RegressionModel()
+
 ElasticNet = RegressionModel()
+
 KNeighborsRegressor = RegressionModel()
+
 PassiveAggressiveRegressor = RegressionModel()
+
 ExtraTreesRegressor = RegressionModel()
+
 XGBRegressor = RegressionModel()
+
 HuberRegressor = RegressionModel()
+
 LGBMRegressor = RegressionModel()
+
 CatBoostRegressor = RegressionModel()
+
 GradientBoostingRegressor = RegressionModel()
+
 # Train models
 regressor_linear.train_linear_regression(X_train, y_train)
+
 regressor_decision_tree.train_decision_tree_regression(X_train, y_train)
+
 regressor_random_forest.train_random_forest_regression(X_train, y_train)
+
 OrthogonalMatchingPursuit.train_orthogonal_matching_pursuit(X_train, y_train)
+
 ElasticNet.train_elastic_net_regression(X_train, y_train)
+
 KNeighborsRegressor.train_knn_regression(X_train, y_train)
-PassiveAggressiveRegressor.train_passive_aggressive_regression(X_train, y_train)
-ExtraTreesRegressor.train_extra_trees_regression(X_train, y_train)
-XGBRegressor.train_xgboost_regression(X_train, y_train)
-HuberRegressor.train_huber_regression(X_train, y_train)
-LGBMRegressor.train_lightgbm_regression(X_train, y_train)
-CatBoostRegressor.train_catboost_regression(X_train, y_train)
-GradientBoostingRegressor.train_gradient_boosting_regression(X_train, y_train)
+
+# PassiveAggressiveRegressor.train_passive_aggressive_regression(X_train, y_train)
+
+# ExtraTreesRegressor.train_extra_trees_regression(X_train, y_train)
+
+# XGBRegressor.train_xgboost_regression(X_train, y_train)
+
+# HuberRegressor.train_huber_regression(X_train, y_train)
+
+# LGBMRegressor.train_lightgbm_regression(X_train, y_train)
+
+# CatBoostRegressor.train_catboost_regression(X_train, y_train)
+
+# GradientBoostingRegressor.train_gradient_boosting_regression(X_train, y_train)
 
 
 
 # Evaluate each model
 
 models = {
-
     "Linear Regression": regressor_linear,
+    
     "Decision Tree Regression": regressor_decision_tree,
+    
     "Random Forest Regression": regressor_random_forest,
+    
     "Orthogonal Matching Pursuit": OrthogonalMatchingPursuit,
+    
     "Elastic Net": ElasticNet,
+    
     "K-Nearest Neighbors": KNeighborsRegressor,
+    
     "Passive Aggressive Regressor": PassiveAggressiveRegressor,
+    
     "Extra Trees Regressor": ExtraTreesRegressor,
+    
     "Extreme Gradient Boosting": XGBRegressor,
+    
     "Huber Regressor": HuberRegressor,
+    
     "Light Gradient Boosting Machine": LGBMRegressor,
+    
     "CatBoost Regressor": CatBoostRegressor,
+    
     "Gradient Boosting Regressor": GradientBoostingRegressor
+
 
 }
 
-# Print trained models
-for name, model in models.items():
-    print(f"{name} Model Instance: {model.regression_model}")
 
 
 metrics_dict = {}
